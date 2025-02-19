2a3bc7222c09543386cd8740e9e542be077de551
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-02-19 19:43:07 +0000

    Integral variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VariousTypes
+{
+
+    public class Program
+    {
+       
+        private static void Main(string[] args)
+        {
+            int number;
+            number = 8;
+
+            int number2 = 9;
+
+            uint number3 = 4U;
+
+            long number4 = 2L;
+
+            ulong number5 = 7UL;
+
+
+            Console.WriteLine("Hello LP1!");
+            Console.WriteLine(number);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+            Console.WriteLine(number4);
+            Console.WriteLine(number5);
+        }
+    }
+}
