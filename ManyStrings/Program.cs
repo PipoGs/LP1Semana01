e142506bc6aeb2c0d02f8be9338d4160d6e2f7ab
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-02-19 21:35:35 +0000

    Added Some String tests

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {   
+            Console.OutputEncoding = Encoding.UTF8;
+            //Strings
+            string string1 = "Hello LP1!";
+            
+            string string2 = "Um tab\t e um copyright\u00a9";
+            string string3 = "This is a star\t\u2727";
+
+            
+
+            Console.WriteLine("Hello LP1!");
+
+            //Strings
+
+            Console.WriteLine(string1);
+            Console.WriteLine(string2);
+            Console.WriteLine(string3);
+        }
+    }
+}
