5d34cd66d4157cde999f60dc7e2dbb4485334e37
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-02-19 22:37:19 +0000

    updated general code

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -12,19 +12,19 @@ namespace AskForInput
             //Ask for input1
             Console.WriteLine("Write a Integer Number:");
 
-            //input conversion1
+            //input conversion15
             string input1 = Console.ReadLine();
-            int convers1 = int.Parse(input1);
+            int conversion1 = int.Parse(input1);
 
             //Ask for input2
             Console.WriteLine("Write a Real Number:");
 
             //input conversion2
             string input2 = Console.ReadLine();
-            float convers2 = float.Parse(input2);
+            float conversion2 = float.Parse(input2);
 
             //Result
-            Console.WriteLine($"{convers1} + {convers2} equals to {convers1 + convers2}");
+            Console.WriteLine($"{conversion1} + {conversion2} equals to {conversion1 + conversion2}");
 
 
 
