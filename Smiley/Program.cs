96384efb321d18f9202cf01c065c70d128f08221
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-02-21 17:31:25 +0000

    Unicode Conversion Start

---
+++ Smiley/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Escreva um numero inteiro em formato hexadecimal: ");
+
+            string input = Console.ReadLine();
+            int conversion1 = int.Parse(input);
+            char unicode = (char)conversion1;
+
+            //char unicode = "$({\u} + {conversion});
+
+           Console.WriteLine(unicode);
+
+            
+            
+        }
+    }
+}
